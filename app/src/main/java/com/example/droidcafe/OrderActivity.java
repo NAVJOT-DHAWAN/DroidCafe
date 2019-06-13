d1871964e513c547d9db5face8caf3cff1dfd795
Navjot Singh <navjotd139@gmail.com>
2019-06-13 16:32:02 +0530

    Code for radiobuttons is added.

--- app/src/main/java/com/example/droidcafe/OrderActivity.java
+++ app/src/main/java/com/example/droidcafe/OrderActivity.java
@@ -3,7 +3,10 @@ package com.example.droidcafe;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -20,4 +23,28 @@ public class OrderActivity extends AppCompatActivity {
     }
 
 
+    public void onRadioButtonClicked(View view) {
+
+        boolean checked = ((RadioButton)view).isChecked();
+
+        switch(view.getId()){
+
+            case R.id.sameday:
+                if(checked)
+                    displayToast(getString(R.string.same));
+                break;
+            case R.id.nextday:
+                if(checked)
+                    displayToast(getString(R.string.next));
+                break;
+            case R.id.pickup:
+                if(checked)
+                    displayToast(getString(R.string.pick));
+                break;
+        }
+    }
+
+    private void displayToast(String string) {
+        Toast.makeText(getApplicationContext(),string,Toast.LENGTH_SHORT).show();
+    }
 }
