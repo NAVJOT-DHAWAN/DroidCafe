38795be32d6c18ffd0ffbfe36e107e05c118afb0
Navjot Singh <navjotd139@gmail.com>
2019-06-14 11:30:54 +0530

    if statement changed

--- app/src/main/java/com/example/droidcafe/MainActivity.java
+++ app/src/main/java/com/example/droidcafe/MainActivity.java
@@ -47,7 +47,7 @@ public  class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if(id == R.id.action_settings){
+        if(id == R.id.action_order){
             return true;
         }
 
