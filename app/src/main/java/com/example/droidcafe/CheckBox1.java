6e7005b290e82da129e33920b0dc30936a588d85
Navjot Singh <navjotd139@gmail.com>
2019-06-14 10:55:47 +0530

    redundant code removed

--- app/src/main/java/com/example/droidcafe/CheckBox1.java
+++ app/src/main/java/com/example/droidcafe/CheckBox1.java
@@ -13,10 +13,17 @@ public class CheckBox1 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        addListenerOnButtonClick();
+        setContentView(R.layout.activity_check_box);
+        //addListenerOnButtonClick();
+        chocolate=(CheckBox)findViewById(R.id.syrup);
+        sprinkles=(CheckBox)findViewById(R.id.sprinkle);
+        nuts=(CheckBox)findViewById(R.id.nuts);
+        cherries=(CheckBox)findViewById(R.id.cherry);
+        cookie=(CheckBox)findViewById(R.id.oreo);
+        buttonOrder=(Button)findViewById(R.id.button);
+
     }
-    public void addListenerOnButtonClick(){
+   /* public void addListenerOnButtonClick(){
 
         chocolate=(CheckBox)findViewById(R.id.syrup);
         sprinkles=(CheckBox)findViewById(R.id.sprinkle);
@@ -25,39 +32,34 @@ public class CheckBox1 extends AppCompatActivity {
         cookie=(CheckBox)findViewById(R.id.oreo);
         buttonOrder=(Button)findViewById(R.id.button);
 
+    }*/
 
-        buttonOrder.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View view) {
-                int totalamount=0;
-                StringBuilder result=new StringBuilder();
-                result.append("Selected Items:");
-                if(chocolate.isChecked()){
-                    result.append("\nChocolate Syrup 100Rs");
-                    totalamount+=100;
-                }
-                if(sprinkles.isChecked()){
-                    result.append("\nSprinkles 50Rs");
-                    totalamount+=50;
-                }
-                if(nuts.isChecked()){
-                    result.append("\nCrushed Nuts 120Rs");
-                    totalamount+=120;
-                }
-                if(cherries.isChecked()){
-                    result.append("\nCherries 200Rs");
-                    totalamount+=200;
-                }
-                if(cookie.isChecked()){
-                    result.append("\nOreo Cookie Crumbles 250Rs");
-                    totalamount+=250;
-                }
-                result.append("\nTotal: "+totalamount+"Rs");
-                //Displaying the message on the toast
-                Toast.makeText(getApplicationContext(), result.toString(), Toast.LENGTH_LONG).show();
-            }
-
-        });
+    public void launch2(View view) {
+        int totalamount=0;
+        StringBuilder result=new StringBuilder();
+        result.append("Selected Items:");
+        if(chocolate.isChecked()){
+            result.append("\nChocolate Syrup 100Rs");
+            totalamount+=100;
+        }
+        if(sprinkles.isChecked()){
+            result.append("\nSprinkles 50Rs");
+            totalamount+=50;
+        }
+        if(nuts.isChecked()){
+            result.append("\nCrushed Nuts 120Rs");
+            totalamount+=120;
+        }
+        if(cherries.isChecked()){
+            result.append("\nCherries 200Rs");
+            totalamount+=200;
+        }
+        if(cookie.isChecked()){
+            result.append("\nOreo Cookie Crumbles 250Rs");
+            totalamount+=250;
+        }
+        result.append("\nTotal: "+totalamount+"Rs");
+        //Displaying the message on the toast
+        Toast.makeText(getApplicationContext(), result.toString(), Toast.LENGTH_LONG).show();
     }
 }
